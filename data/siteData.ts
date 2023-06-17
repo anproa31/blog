3954c987b6a1182ffa9caedf5d8756aa405d3c76
anproa31 <antoeta31@gmail.com>
2023-06-17 16:05:39 +0700

    blog

---
+++ data/siteData.ts
@@ -0,0 +1,17 @@
+const siteData = {
+  title: 'An Buiii\'s Blog',
+  author: 'Notion Blog',
+  headerTitle: 'An Quoc Bui',
+  profileUrl: '/nextjs-logo.png',
+  headerDescription: 'Notion Blog for Android, Web, AI & Cloud Developers',
+  footerText: '© All rights reserved',
+  language: 'en-us',
+  locale: 'en-US',
+  socialBanner: 'https://nextjs-notion-blog-starter.vercel.app/preview/1.jpg',
+  github: 'https://github.com/anproa31',
+  twitter: 'https://twitter.com/BuiiiAn',
+  websiteUrl: 'https://portfolio-two-delta-82.vercel.app/',
+  linkedin: 'https://www.linkedin.com/in/bui-an/'
+};
+
+export default siteData;
